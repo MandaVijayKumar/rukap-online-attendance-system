6d985e2c0b7bed4a7eeb278ec878fa16d85d0d76
MandaVijayKumar <96509400+MandaVijayKumar@users.noreply.github.com>
2026-03-26 18:08:17 +0530

    Refactor time formatting logic in RegistrarLiveClasses

--- src/pages/registrar/RegistrarLiveClasses.jsx
+++ src/pages/registrar/RegistrarLiveClasses.jsx
@@ -171,20 +171,13 @@ borderBottom:"2px solid #1565c0"
 <TableBody>
 
 {group.map((cls,index)=>{
+
 const formattedTime = cls.time
-  ? new Date(`1970-01-01T${cls.time}`).toLocaleTimeString("en-IN", {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-      timeZone: "Asia/Kolkata"
-    })
-  : "-";
-// const formattedTime = cls.time
-// ? new Date(`1970-01-01T${cls.time}`).toLocaleTimeString("en-IN",{
-// hour:"2-digit",
-// minute:"2-digit"
-// })
-// : "-";
+? new Date(`1970-01-01T${cls.time}`).toLocaleTimeString("en-IN",{
+hour:"2-digit",
+minute:"2-digit"
+})
+: "-";
 
 return(
 
